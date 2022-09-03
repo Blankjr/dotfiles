f52bda261b2148b28e8b0ae4fd58d8b2679bd565
Christian <blankj.me@gmail.com>
2022-09-03 02:24:45 +0200

    redshift nightsettings

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -23,7 +23,7 @@ run volumeicon
 #you can set wallpapers in themes as well
 # feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
 #run applications from startup
-run redshift
+run redshift-gtk
 run mullvad
 run copyq
 # run setxkbmap eu  
