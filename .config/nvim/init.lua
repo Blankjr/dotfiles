36644a2d46371aee90b7140639bab7e7595e7f7f
Christian <blankj.me@gmail.com>
2022-09-24 16:27:27 +0200

    Feature: Git Signs & Relative Numbers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,10 @@
 vim.bo.expandtab = true
 vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 2
+
+-- Relative Numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
 -- Leader Space
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
@@ -18,18 +22,21 @@ keymap('n', '<c-k>', '<c-w>k', opts)
 keymap('n', '<c-l>', '<c-w>l', opts)
 -- Plugin Keymaps
 keymap('n', '<leader>gg', ':LazyGit<CR>', {})
-
+-- Plugin Management
 require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use { "catppuccin/nvim", as = "catppuccin" }
   use { "kdheepak/lazygit.nvim" }
+  use {
+  'lewis6991/gitsigns.nvim',
+  -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+  }
 end)
-
-
+-- Theme Setup
 vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
-
 require("catppuccin").setup()
-
 vim.cmd [[colorscheme catppuccin]]
+
+require('gitsigns').setup()
 
