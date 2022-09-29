da9c0eded273406dc9f3a42c64268fe420cfc6e8
Christian <blankj.me@gmail.com>
2022-09-29 15:45:32 +0200

    Feature: Null-ls format with Prettier, StyLua on LEADER + bf

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -1,23 +1,34 @@
 local keymap = vim.api.nvim_set_keymap
 -- Save
-keymap('n', '<c-s>', ':w<CR>', {})
-keymap('i', '<c-s>', '<ESC>:w<CR>a', {})
-keymap('v', '<leader>yy', '+yy', {})
+keymap("n", "<c-s>", ":w<CR>", {})
+keymap("i", "<c-s>", "<ESC>:w<CR>a", {})
+keymap("v", "<leader>yy", "+yy", {})
 local opts = { noremap = true }
 -- Fast Escape insert mode
-keymap('i', 'jj', '<ESC>', {})
+keymap("i", "jj", "<ESC>", {})
 -- Mouse
-vim.cmd[[set mouse+=a]]
+vim.cmd([[set mouse+=a]])
 -- Move between splits with CTRL + hjkl
-keymap('n', '<c-j>', '<c-w>j', opts)
-keymap('n', '<c-h>', '<c-w>h', opts)
-keymap('n', '<c-k>', '<c-w>k', opts)
-keymap('n', '<c-l>', '<c-w>l', opts)
+keymap("n", "<c-j>", "<c-w>j", opts)
+keymap("n", "<c-h>", "<c-w>h", opts)
+keymap("n", "<c-k>", "<c-w>k", opts)
+keymap("n", "<c-l>", "<c-w>l", opts)
 -- Plugin Keymaps
-keymap('n', '<leader>gg', ':LazyGit<CR>', {})
+keymap("n", "<leader>gg", ":LazyGit<CR>", {})
 -- Telescope
-keymap('n', '<leader>ff', ':Telescope find_files<CR>', {})
-keymap('n', '<leader>fg', ':Telescope live_grep<CR>', {})
-keymap('n', '<leader>fb', ':Telescope buffers<CR>', {})
-keymap('n', '<leader>fh', ':Telescope help_tags<CR>', {})
+keymap("n", "<leader>ff", ":Telescope find_files<CR>", {})
+keymap("n", "<leader>fg", ":Telescope live_grep<CR>", {})
+keymap("n", "<leader>fb", ":Telescope buffers<CR>", {})
+keymap("n", "<leader>fh", ":Telescope help_tags<CR>", {})
 
+--local util = require 'vim.lsp.util'
+--local formatting_callback = function(client, bufnr)
+--  vim.keymap.set('n', '<leader>bf', function()
+--    local params = util.make_formatting_params({})
+--    client.request('textDocument/formatting', params, nil, bufnr) 
+--  end, {buffer = bufnr})
+--end
+--
+--
+-- Null LS Format
+keymap("n", "<leader>bf", ":lua vim.lsp.buf.formatting()<CR>", {})
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,62 +1,92 @@
 -- Plugin Management
 --local lspconfig = require "plugins.confs.lspconfs"
-local cmp = require "plugins.confs.cmp"
+local cmp = require("plugins.confs.cmp")
 
-require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  }
-  use(cmp)
-  -- use { "catppuccin/nvim", as = "catppuccin" }
-  use "EdenEast/nightfox.nvim"
-  use { "kdheepak/lazygit.nvim" }
-  use {
-    'lewis6991/gitsigns.nvim',
-    -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
-  }
-  use 'andweeb/presence.nvim'
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    -- or                            , branch = '0.1.x',
-    requires = { { 'nvim-lua/plenary.nvim' } },
-  }
-  use {
-    "max397574/better-escape.nvim",
-    config = function()
-      require("better_escape").setup(
-      )
-    end,
-  }
-  use { 'lukas-reineke/indent-blankline.nvim',
-    requires = 'nvim-treesitter/nvim-treesitter',
-    config = function()
-      require("indent_blankline").setup {
-        space_char_blankline = " ",
-        show_current_context = true,
-      }
-    end
-  }
-  use {
-    'numToStr/Comment.nvim',
-    config = function()
-      require('Comment').setup()
-    end
-  }
-  use {
-    'romgrk/barbar.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons' }
-  }
+require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
+	use("neovim/nvim-lspconfig") -- Configurations for Nvim LSP
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+	})
+	use(cmp)
+	-- use { "catppuccin/nvim", as = "catppuccin" }
+	use("EdenEast/nightfox.nvim")
+	use({ "kdheepak/lazygit.nvim" })
+	use({
+		"lewis6991/gitsigns.nvim",
+		-- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+	})
+	use("andweeb/presence.nvim")
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use({
+		"max397574/better-escape.nvim",
+		config = function()
+			require("better_escape").setup()
+		end,
+	})
+	use({
+		"lukas-reineke/indent-blankline.nvim",
+		requires = "nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("indent_blankline").setup({
+				space_char_blankline = " ",
+				show_current_context = true,
+			})
+		end,
+	})
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	})
+	use({
+		"romgrk/barbar.nvim",
+		requires = { "kyazdani42/nvim-web-devicons" },
+	})
 
+	use({
+		"jose-elias-alvarez/null-ls.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+	})
 end)
 -- Theme Setup
 -- vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
 -- require("catppuccin").setup()
 -- vim.cmd [[colorscheme catppuccin]]
 vim.cmd("colorscheme nightfox")
-require('gitsigns').setup()
+require("gitsigns").setup()
 --use(lspconfig)
-require'lspconfig'.pyright.setup{}
+-- Use an on_attach function to only map the following keys
+on_attach = function(client)
+    client.resolved_capabilities.document_formatting = false
+end
+--require'lspconfig'.bashls.setup{}
+local lsp_flags = {
+	-- This is the default in Nvim 0.7+
+	debounce_text_changes = 150,
+}
+require("lspconfig")["pyright"].setup({
+	flags = lsp_flags,
+})
+require("lspconfig")["tsserver"].setup({
+	on_attach = on_attach,
+	flags = lsp_flags,
+})
+require("null-ls").setup({
+	sources = {
+		require("null-ls").builtins.formatting.stylua,
+		require("null-ls").builtins.formatting.prettier,
+		--require("null-ls").builtins.diagnostics.eslint,
+		require("null-ls").builtins.completion.spell,
+	},
+})
